ce36dba73b18b69f05ba818c522d1b55201248c7
Thomas <thomaspmar@protonmail.com>
2021-04-17 09:59:59 -0700

    Uploaded initial files

---
+++ Forex Price Viewer.Shared/PriceData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Forex_Price_Viewer
+{
+    public class PriceData
+    {
+        public string Ticker { get; set; }
+        public DateTime Date { get; set; }
+        public int Time { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+    }
+}
