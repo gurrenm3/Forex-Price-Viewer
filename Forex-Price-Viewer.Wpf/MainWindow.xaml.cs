29b3ef1d3828ec6547e237f5471606470fc8cdf4
nacho481 <nachitoatilano@gmail.com>
2021-04-17 11:53:01 -0700

    Intro page to app
    Have the user enter the date and time to pull up data for Forex Price Viewer. Just the GUI.

--- Forex-Price-Viewer.Wpf/MainWindow.xaml.cs
+++ Forex-Price-Viewer.Wpf/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Forex_Price_Viewer.Wpf
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
