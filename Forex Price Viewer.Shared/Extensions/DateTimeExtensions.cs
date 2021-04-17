3387a0fc79ef4eea60a1ee00a626e3276d9abc3a
Thomas <thomaspmar@protonmail.com>
2021-04-17 11:30:45 -0700

    Added Shared Project

---
+++ Forex Price Viewer.Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Forex_Price_Viewer.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToUrlFileName(this DateTime dt)
+        {
+            return $"{dt.Year}/" +
+                $"{dt.Month.ToString().PadLeft(2, '0')}/" +
+                $"{dt.Day.ToString().PadLeft(2, '0')}" +
+                $"{dt.Month.ToString().PadLeft(2, '0')}" +
+                $"{dt.Year.ToString().Substring(2, 2)}";
+        }
+    }
+}
---
+++ Forex Price Viewer.Shared/ForexDownloader.cs
@@ -0,0 +1,25 @@
+﻿using Forex_Price_Viewer.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Forex_Price_Viewer.Shared
+{
+    public class ForexDownloader
+    {
+        public const string baseUrl = "https://www.forexite.com/free_forex_quotes/";
+
+        public void Test()
+        {
+            var date = DateTime.Today.AddDays(-1).ToUrlFileName();
+            //var date = DateToUrlFileName(DateTime.Now.AddHours(-1));
+
+            /*string urlDt = DateToUrlFileName(DateTime.Today);
+            string url = $"https://www.forexite.com/free_forex_quotes/{urlDt}.zip";*/
+
+            
+            Process.Start($"{baseUrl}{date}.zip");
+        }
+    }
+}
